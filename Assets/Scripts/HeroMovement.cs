3b693da6790ce705b3ea12e9d65953c29f1536df
KovalevMO <kovalev_mo@icloud.com>
2021-12-13 06:36:30 +0300

    Initial commit

---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Vector3 _direction;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        Debug.Log(_direction);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed * 2;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public int Number;
+    public Transform Position;
+    public GameObject EnemyPrefab;
+
+    void Start()
+    {
+        var g01 = Instantiate(EnemyPrefab, Position.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
